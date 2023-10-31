a44246a98d9853e44666dc24895e812bc9a7acb8
Nikita Mercury <lking1231@mail.ru>
2023-10-31 19:52:30 +0300

    Added mainMenu()

--- Model.java
+++ Model.java
@@ -1,3 +1,25 @@
 public class Model {
 	
+	public static void mainMenu() {
+		boolean isClose = false;
+		while (!isClose) {
+			int chosen = View.printMainMenu();
+			switch (chosen) {
+				case 1:
+					// .Start new game
+					break;
+				case 2:
+					View.printHelpPage();
+					break;
+				case 3:
+					isClose = true;
+					break;
+				default:
+					System.out.println("Wrong input!");
+					break;
+			}
+		}
+		
+
+	}
 }
