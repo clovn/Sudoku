f872cb1acaa0d1837649fef2108199c1b262bd7e
Nikita Mercury <lking1231@mail.ru>
2023-10-31 18:44:48 +0300

    Added method printMainMenu()

--- View.java
+++ View.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class View {
 	
+	public static int printMainMenu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("===== Главное меню =====");
+		System.out.println("1. Играть");
+		System.out.println("2. Помощь");
+		System.out.println("3. Выйти");
+		int ch = sc.nextInt();
+		return ch;
+	}
+
+
+
 }
