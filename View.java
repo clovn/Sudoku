d14343eff2191ea06521ab47ae4f8359989d32d3
Clovn <Haliullin101@mail.ru>
2023-10-31 23:05:40 +0300

    update methods printHelpPage, printMainMenu in View

--- View.java
+++ View.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 public class View {
+	private static Scanner sc = new Scanner(System.in);
 	
 	public static int printMainMenu() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("===== Главное меню =====");
-		System.out.println("1. Играть");
-		System.out.println("2. Помощь");
-		System.out.println("3. Выйти");
+		String text = "===== Главное меню =====\n" +
+		"1. Играть\n" +
+		"2. Помощь\n" +
+		"3. Выйти\n";
+		refresh(text);
 		int ch = sc.nextInt();
 		return ch;
 	}
 
 	public static void printHelpPage() {
-		System.out.println("===== Правила =====");
-		System.out.println("-От вас требуется заполнить свободные клетки цифрами от 1 до 9 так,\nчтобы в каждой строке, в каждом столбце и в каждом малом квадрате 3*3\nкаждая цифра встречалась бы только один раз");
-		System.out.println("===== Управление =====");
-		System.out.println("-Чтобы поставить цифру, вам нужно выбрать сектор от 1 до 9.\n-Ввести координаты места через пробел.(1 1 или 2 3 или 3 1 и т.д.).");
-		System.out.println("-Ввести цифру которую, хотите вставить.");
+		String text = "===== Правила =====\n" +
+		"-От вас требуется заполнить свободные клетки цифрами от 1 до 9 так,\nчтобы в каждой строке, в каждом столбце и в каждом малом квадрате 3*3\nкаждая цифра встречалась бы только один раз\n" +
+		"===== Управление =====\n" +
+		"-Чтобы поставить цифру, вам нужно выбрать сектор от 1 до 9.\n-Ввести координаты места через пробел.(1 1 или 2 3 или 3 1 и т.д.).\n" +
+		"-Ввести цифру которую, хотите вставить.\n" +
+		"(чтобы вернуться назад введите любую цифру)\n";
+		refresh(text);
+		sc.nextInt();
 	}
 
 	public static void printGameResult(boolean win) {
@@ -27,7 +31,8 @@ public class View {
 		}
 	}
 
-	public static void refresh(){
+	public static void refresh(String text){
 		System.out.print("\033\143");
+		System.out.print(text);
 	}
 }
