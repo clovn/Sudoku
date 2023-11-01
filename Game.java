0edd1ee2e917b4dbb9009a0e73741f35acd6d04a
clovn <haliullin101@mail.ru>
2023-11-01 23:26:38 +0300

    added method generateField(not ended), added methods validate, checkEndCondition, start, printGameField

--- Game.java
+++ Game.java
@@ -1,3 +1,73 @@
+import java.util.Arrays;
+
 public class Game {
-	
+	private int[][] gameField = new int[9][9];
+    //private int[][] rightField = new int[9][9];
+
+    public Game(){
+    	generateGameField();
+    }
+
+    public void start() {
+    	boolean isLose = false;
+
+    	while(!checkEndCondition()){
+    		int[] guess = View.printGameField(gameField);
+
+    		if(!validate(guess)){
+    			continue;
+    		}
+
+    		setDigit(guess);
+    	}
+
+//    	for(int i = 0; i < 9; i++){
+//    		if(!Arrays.equals(rightField[i], gameField[i])){
+//    			isLose = true;
+//    			break;
+//    		}
+//    	}
+
+    	View.printGameResult(!isLose);
+    }
+
+    private void setDigit(int[] guess){
+        gameField[guess[0]/3*3 + guess[2]][guess[0]%3*3 + guess[1]] = guess[3];
+    }
+
+    private int getDigit(int[] guess){
+    	return gameField[guess[0]%3*3 + guess[1]][guess[0]/3*3 + guess[2]];
+    }
+
+    private void generateGameField(){
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+            	if(Math.random() < 0.5){
+                	gameField[i][j] = (i*3 + i/3 + j) % 9 + 1;
+            	}
+            }
+        }
+
+
+    }
+
+    private boolean validate(int[] guess){
+    	for(int i = 0; i < 9; i++){
+    		if(getDigit(guess) != 0 && gameField[guess[1]][i] != guess[3] && gameField[i][guess[2]] != guess[3] && gameField[guess[0]/3*3 + i/3][guess[0]%3*3 + i%3] != guess[3]) return false;
+    	}
+
+    	return true;
+    }
+
+	private boolean checkEndCondition(){
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				if(gameField[i][j] == 0){
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
 }
--- View.java
+++ View.java
@@ -1,26 +1,37 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class View {
-	private static Scanner sc = new Scanner(System.in);
 	
 	public static int printMainMenu() {
-		String text = "===== Главное меню =====\n" +
-		"1. Играть\n" +
-		"2. Помощь\n" +
-		"3. Выйти\n";
+		Scanner sc = new Scanner(System.in);
+		String text = """
+				===== Главное меню =====
+				1. Играть
+				2. Помощь
+				3. Выйти
+				""";
 		refresh(text);
-		int ch = sc.nextInt();
-		return ch;
+		return sc.nextInt();
 	}
 
 	public static void printHelpPage() {
-		String text = "===== Правила =====\n" +
-		"-От вас требуется заполнить свободные клетки цифрами от 1 до 9 так,\nчтобы в каждой строке, в каждом столбце и в каждом малом квадрате 3*3\nкаждая цифра встречалась бы только один раз\n" +
-		"===== Управление =====\n" +
-		"-Чтобы поставить цифру, вам нужно выбрать сектор от 1 до 9.\n-Ввести координаты места через пробел.(1 1 или 2 3 или 3 1 и т.д.).\n" +
-		"-Ввести цифру которую, хотите вставить.\n" +
-		"(чтобы вернуться назад введите любую цифру)\n";
+		Scanner sc = new Scanner(System.in);
+
+		String text = """
+				===== Правила =====
+				-От вас требуется заполнить свободные клетки цифрами от 1 до 9 так,
+				чтобы в каждой строке, в каждом столбце и в каждом малом квадрате 3*3
+				каждая цифра встречалась бы только один раз
+				===== Управление =====
+				-Чтобы поставить цифру, вам нужно выбрать сектор от 1 до 9.
+				-Ввести координаты места через пробел.(1 1 или 2 3 или 3 1 и т.д.).
+				-Ввести цифру которую, хотите вставить.
+				(чтобы вернуться назад введите любую цифру)
+				""";
 		refresh(text);
-		sc.nextInt();
+
+		sc.nextLine();
 	}
 
 	public static void printGameResult(boolean win) {
@@ -32,7 +43,27 @@ public class View {
 	}
 
 	public static void refresh(String text){
-		System.out.print("\033\143");
+		System.out.print("\033[H\033[2J");
 		System.out.print(text);
+	}
+
+	public static int[] printGameField(int[][] field) {
+		String text = "---------------------\n";
+		Scanner sc = new Scanner(System.in);
+
+		for(int[] line : field) {
+			text += "| ";
+			for(int dg : line) {
+				text += dg + " ";
+			}
+			text += "|\n";
+		}
+
+		text += "---------------------\n";
+
+		refresh(text);
+		System.out.print("Введите ход: ");
+
+		return Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 	}
 }
